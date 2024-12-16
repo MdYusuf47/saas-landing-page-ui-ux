61e2b31d51f08e03c266d819786bdb2b078cb343
Md Yusuf <mdyusuf742171@gmail.com>
2024-12-16 23:23:06 +0530

    create button

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logoImage from "@/assets/images/logo.svg";
+import Button from "@/components/Button";
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -12,11 +13,20 @@ export default function Navbar() {
     return (
         <section className="py-4">
             <div className="container">
-                <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2items-center">
                     <div>
-                        <Image src={logoImage} alt="Layers logo" className="h-9 w-auto"/>
+                        <Image
+                            src={logoImage}
+                            alt="Layers logo"
+                            className="h-9 w-auto md:h-auto"
+                        />
                     </div>
-                    <div className="flex justify-end">
+                    <div>
+                        <nav>
+                            
+                        </nav>
+                    </div>
+                    <div className="flex justify-end gap-4">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
@@ -24,15 +34,17 @@ export default function Navbar() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            className="feather feather-menu"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="feather feather-menu md:hidden"
                         >
                             <line x1="3" y1="12" x2="21" y2="12"></line>
                             <line x1="3" y1="6" x2="21" y2="6"></line>
                             <line x1="3" y1="18" x2="21" y2="18"></line>
                         </svg>
+                        <Button variant="secondary">Log In</Button>
+                        <Button variant="primary">Sign Up</Button>
                     </div>
                 </div>
             </div>
