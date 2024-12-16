d51d54b0c4ed810eb212873f784b2895da383227
Md Yusuf <mdyusuf742171@gmail.com>
2024-12-16 22:54:56 +0530

    create nav mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-    return <div>Hello World</div>;
+import Navbar from '@/sections/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
 }
+
+export default page
