e5da9d556472b97dc770affe4861b7920d12a6e9
Md Yusuf <mdyusuf742171@gmail.com>
2024-12-17 11:51:24 +0530

    create tablet view hero section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,17 +7,21 @@ const classes = cva("border  h-12 rounded-full px-6 font-medium", {
             primary: "bg-lime-400 text-neutral-950 border-lime-400",
             secondary: "border-white text-white bg-transparent",
         },
+        size:{
+            sm:'h-10',
+        }
     },
 });
 
 const Button = (
     props: {
         variant: "primary" | "secondary";
+        size?:'sm'
     } & ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
-    const { variant, className, ...otherProps } = props;
+    const { variant,size, className, ...otherProps } = props;
     return (
-        <button className={classes({ variant, className })} {...otherProps} />
+        <button className={classes({ variant,size, className })} {...otherProps} />
     );
 };
 
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,15 +10,15 @@ const Hero = () => {
                         ✨ $7.5M seed round raised
                     </div>
                 </div>
-                <h1 className="text-6xl font-medium text-center mt-6">Impactful design, created effortlessly</h1>
-                <p className="text-center text-xl text-white/50 mt-8">
+                <h1 className="text-6xl font-medium text-center mt-6 md:text-7xl">Impactful design, created effortlessly</h1>
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
                     Design tools shouldn&apos;t slow you down.Layers combine
                     powerful features with an intuitive interface that keep you
                     in your creative flow.
                 </p>
-                <form className="flex border border-white/15 rounded-full p-2 mt-8">
-                    <input type="email" placeholder="Enter your email" className="bg-transparent px-4"/>
-                    <Button type="submit" variant="primary" className="whitespace-nowrap">Sign Up</Button>
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
+                    <input type="email" placeholder="Enter your email" className="bg-transparent px-4 md:flex-1"/>
+                    <Button type="submit" variant="primary" size="sm" className="whitespace-nowrap">Sign Up</Button>
                 </form>
             </div>
         </section>
