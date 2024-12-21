87c9656faeb2a393530ce6f9f982ec27f7fbec9f
Md Yusuf <mdyusuf742171@gmail.com>
2024-12-21 10:53:27 +0530

    add mouse pointer component and integrate into Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,22 +3,29 @@ import React from "react";
 import designExample1Image from "@/assets/images/design-example-1.png";
 import designExample2Image from "@/assets/images/design-example-2.png";
 import Image from "next/image";
+import Pointer from "@/components/Pointer";
 
 const Hero = () => {
     return (
         <section className="py-24 overflow-x-clip">
             <div className="container relative">
-                <div className="absolute -left-32 top-16">
+                <div className="absolute -left-32 top-16 hidden lg:block">
                     <Image
                         src={designExample1Image}
                         alt="Design example 1 image"
                     />
                 </div>
-                <div className="absolute -right-64 -top-16">
+                <div className="absolute -right-64 -top-16 hidden lg:block">
                     <Image
                         src={designExample2Image}
                         alt="Design example 2 image"
                     />
+                </div>
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name="Andrea"/>
+                </div>
+                <div className=" absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name="Bryan" color="red"/>
                 </div>
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
