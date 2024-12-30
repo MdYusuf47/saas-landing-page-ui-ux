e386ade70b1906eb603f2804814dea3cdd5cba66
Md Yusuf <mdyusuf742171@gmail.com>
2024-12-30 11:02:38 +0530

    create features section structure

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -9,5 +9,52 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return (
+        <section>
+            <div className="container">
+                <h2>
+                    Where power meets <span>simplicity</span>
+                </h2>
+                <div>
+                    <div>
+                        <div></div>
+                        <div>
+                            <h3>Real-time Collaboration</h3>
+                            <p>
+                                Work together seamlessly with conflict-free team
+                                editing
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div></div>
+                        <div>
+                            <h3>Interactive Prototyping</h3>
+                            <p>
+                                Engage your client with prototype that react to
+                                user actions
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                    <div></div>
+                        <div>
+                            <h3>Keyboard Quick Actions</h3>
+                            <p>
+                                Powerful commands to help you create designs more quickly
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {features.map((feature) => (
+                        <div key={feature}>
+                            <span></span>
+                            <span>{feature}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
